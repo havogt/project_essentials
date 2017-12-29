37a8ac03494aa7f074abfc5e2b9147f991d90302
Hannes Vogt <hannes@havogt.de>
2017-12-30 00:14:39 +0100

    download to toplevel build dir

--- googletest-download/googletest-download.cmake
+++ googletest-download/googletest-download.cmake
@@ -1,5 +1,5 @@
 # Download and unpack googletest at configure time
-configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in googletest-download/CMakeLists.txt)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt.in ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
 execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
     WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/googletest-download" )
 execute_process(COMMAND "${CMAKE_COMMAND}" --build .
